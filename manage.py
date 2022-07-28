23e927890a3414798099991d6f60a4e0b08bd7f7
Limo Kiplimo <limo@mega.com>
2022-07-28 10:26:26 +0300

    Updated

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
-def main():
+def main( ):
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contactsapi.settings')
     try:
@@ -19,4 +19,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main( )
